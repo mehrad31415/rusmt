43fd9beaf1b0a30d8e27a60170865527f14f9eab
mehrad31415 <m.haghshenas@students.uu.nl>
2025-01-28 10:47:13 -0500

    28January

--- smt/testing/tests/model/parser/example_ok.rs
+++ smt/testing/tests/model/parser/example_ok.rs
@@ -1,13 +1,18 @@
 use rusmart_smt_remark_derive::{smt_axiom, smt_impl, smt_spec};
 use rusmart_smt_stdlib::{Boolean, Integer, Text, SMT};
 
-#[smt_impl]
+#[smt_spec]
 fn add_two_nums(x: Integer, y: Integer) -> Integer {
-    let a = x.add(y);
-    let b = a.add(Integer::from(1));
-    let c = b;
-    let d = c;
-    c
+    unimplemented!()
 }
+/*
+let a = MyStruct {
+    x: 1,
+    y: 2,
+    z: 3,
+};
+
+let b = a.x;
 
 
+*/

fe1d9a9ef7e4f6a63dee4b9aa0c053e12fdf4e0f
mehrad31415 <m.haghshenas@students.uu.nl>
2025-02-11 19:15:48 -0500

    derive parser complete

--- smt/testing/tests/model/parser/example_ok.rs
+++ smt/testing/tests/model/parser/example_ok.rs
@@ -1,9 +1,25 @@
-use rusmart_smt_remark_derive::{smt_axiom, smt_impl, smt_spec};
-use rusmart_smt_stdlib::{Boolean, Integer, Text, SMT};
+use rusmart_smt_remark_derive::{smt_axiom, smt_impl, smt_spec, smt_type};
+use rusmart_smt_stdlib::{Boolean, Integer, Seq, Text, SMT};
 
 #[smt_spec]
-fn add_two_nums(x: Integer, y: Integer) -> Integer {
-    unimplemented!()
+fn add_two_nums() -> Integer {
+    let x : Integer = 1.into();
+    Integer::from(2)
+}
+
+
+#[smt_type]
+enum MyStruct {
+    Integer1,
+    Integer2,
+    Integer3,
+}
+
+impl MyStruct {
+    #[smt_impl]
+    fn new() -> Self {
+        MyStruct::Integer1
+    }
 }
 /*
 let a = MyStruct {
